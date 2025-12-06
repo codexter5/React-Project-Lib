5bb16c7337c6161173ca22ad3e0d3a0d50126f82
Joker <chumalungchamling@gmail.com>
2025-12-06 19:10:35 +0545

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base:"/React-Project-Library",
+  base:"/React-Project-Lib",
   server: {
     port: 3000,
     proxy: {
